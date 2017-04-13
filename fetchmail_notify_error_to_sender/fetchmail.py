b0e2528c8bc5bf19244350c4008202a1febc5422
mreficent <miquel.raich@eficent.com>
2017-04-13 10:25:12 +0200

    [MIG] fetchmail_notify_error_to_sender

--- fetchmail_notify_error_to_sender/fetchmail.py
+++ fetchmail_notify_error_to_sender/fetchmail.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
 
@@ -25,6 +9,6 @@ class fetchmail_server(models.Model):
 
     _inherit = 'fetchmail.server'
     error_notice_template_id = fields.Many2one(
-        'email.template', string="Error notice template",
+        'mail.template', string="Error notice template",
         help="Set here the template to use to send notice to sender when "
              "errors occur while fetching email")
